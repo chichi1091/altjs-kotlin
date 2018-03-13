f57c495407c456ddabd1b1e0d5565f9170e5746d
ChiharuTerashima <chichi.pa2.1091@gmail.com>
2018-03-14 00:16:45 +0900

    SpringBootはJavaで。AltJSはKotlinに変更

---
+++ src/main/java/com/example/altjskotlin/AltjsKotlinApplication.java
@@ -0,0 +1,11 @@
+package com.example.altjskotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltjsKotlinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AltjsKotlinApplication.class, args);
+    }
+}

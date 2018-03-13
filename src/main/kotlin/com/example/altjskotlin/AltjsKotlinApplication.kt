ab681eab3a090c429459a8effffaa2ec4ff35f20
ChiharuTerashima <chichi.pa2.1091@gmail.com>
2018-03-13 23:56:01 +0900

    created SpringBoot Project

---
+++ src/main/kotlin/com/example/altjskotlin/AltjsKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.altjskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AltjsKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<AltjsKotlinApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/altjskotlin/controller/IndexController.kt
@@ -0,0 +1,15 @@
+package com.example.altjskotlin.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("greeting")
+class IndexController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
+}
